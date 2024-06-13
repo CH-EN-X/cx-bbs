5335d5da10d3042e1e015fa245c07f89c1578b86
CH-EN-X <1211931367@qq.com>
2024-06-14 00:36:00 +0800

    修改常量类

---
+++ cx-bbs-common/src/main/java/com/cx/common/constants/ArticleConstants.java
@@ -0,0 +1,10 @@
+package com.cx.common.constants;
+
+public class ArticleConstants {
+    public static final Short LOADTYPE_LOAD_MORE = 1;
+//    public static final Short LOADTYPE_LOAD_FOLLOW = 1;
+    public static final Short LOADTYPE_LOAD_NEW = 2;
+//    public static final Short LOADTYPE_LOAD_HOT = 2;
+    public static final String DEFAULT_TAG = "__all__";
+
+}
