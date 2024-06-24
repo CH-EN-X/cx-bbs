46166daa2747d3ae380e7179b45ce1a498b21a6d
CH-EN-X <1211931367@qq.com>
2024-06-24 23:16:46 +0800

    实现user的feign远程调用

---
+++ cx-bbs-feign-api/src/main/java/com/cx/feign/client/UserClient.java
@@ -0,0 +1,14 @@
+package com.cx.feign.client;
+
+import com.cx.model.common.dtos.ResponseResult;
+import com.cx.model.user.vo.UserVO;
+import io.swagger.models.auth.In;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "cx-bbs-user") // 指定要调用的服务名
+public interface UserClient {
+    @GetMapping("/api/user/{id}") // 远程调用的接口地址
+    ResponseResult<UserVO> getUser(@PathVariable("id") String id); // 定义对应的方法，用于远程调用获取用户信息
+}
