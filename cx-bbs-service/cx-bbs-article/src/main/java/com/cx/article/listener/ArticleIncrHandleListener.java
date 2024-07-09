77417770c38c948c373277bfe8a922dcd5c87ece
CH-EN-X <1211931367@qq.com>
2024-07-09 17:56:54 +0800

    定义配置类，使用kafka定义监听器作为消费者，消费消息队列的消息，实现消息接收

---
+++ cx-bbs-service/cx-bbs-article/src/main/java/com/cx/article/listener/ArticleIncrHandleListener.java
@@ -0,0 +1,32 @@
+package com.cx.article.listener;
+
+import com.alibaba.fastjson.JSON;
+import com.cx.article.service.ArticleService;
+
+import com.cx.common.constants.HotArticleConstants;
+import com.cx.model.mess.ArticleVisitStreamMess;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ArticleIncrHandleListener {
+
+    @Autowired
+    private ArticleService apArticleService;
+
+    @KafkaListener(topics = HotArticleConstants.HOT_ARTICLE_INCR_HANDLE_TOPIC)
+    public void onMessage(String mess){
+        if(StringUtils.isNotBlank(mess)){
+            ArticleVisitStreamMess articleVisitStreamMess = JSON.parseObject(mess, ArticleVisitStreamMess.class);
+            apArticleService.updateScore(articleVisitStreamMess);
+
+        }
+//        if(StringUtils.isNotBlank(mess)){
+//            System.out.println("----------"+mess);
+//        }
+    }
+}
